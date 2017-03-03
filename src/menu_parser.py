dde570e7d2b182c10cd14d2b2f506a2bde218bff
Simon Rehwald <simon.rehwald@tum.de>
2017-03-04 00:09:47 +0100

    add method for creating a JSON array from a list of menus

--- src/menu_parser.py
+++ src/menu_parser.py
@@ -29,11 +29,6 @@ class Menu:
         self.menu_date = menu_date
         self.dishes = dishes
 
-    def to_json(self):
-        json_str = json.dumps({"date": str(self.menu_date), "dishes": [dish.__dict__ for dish in self.dishes]},
-                              ensure_ascii=False, indent=4)
-        return json_str
-
     def __repr__(self):
         menu_str = str(self.menu_date) + ": " + str(self.dishes)
         return menu_str
--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+import json
 
 date_pattern = "%d.%m.%Y"
 cli_date_format = "dd.mm.yyyy"
@@ -8,3 +9,11 @@ cli_date_format = "dd.mm.yyyy"
 
 def parse_date(date_str):
     return datetime.strptime(date_str, date_pattern).date()
+
+
+def to_json(menus):
+    menu_jsons = json.dumps(
+        [{"date": str(menu.menu_date), "dishes": [dish.__dict__ for dish in menu.dishes]} for menu in menus],
+        ensure_ascii=False, indent=4)
+
+    return menu_jsons
