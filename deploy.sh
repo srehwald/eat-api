b886238d9f7922e95093d28b8a5034497eb55ffe
Simon Rehwald <simon.rehwald@tum.de>
2017-03-05 14:36:42 +0100

    add wrapper script for parsing and deploying

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ remote_repo="https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" && \
 remote_branch="gh-pages" && \
 git init && \
 git config user.name "Travis CI" && \
-git config user.email "travis@stwm-mensa-api.org" && \
+git config user.email "travis@srehwald.github.io" && \
 git add . && \
 git commit -m'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
