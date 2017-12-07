44f6e7f8b44e1351d430f6f8bff9b02d1f7fb8aa
Simon Rehwald <simon.rehwald@tum.de>
2017-12-08 00:26:07 +0100

    implement locations dropdown

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var locations = ["fmi-bistro", "ipp-bistro", "mensa-arcisstr", "mensa-garching", "mensa-leopoldstr", "mensa-lothstr", 
+                 "mensa-martinsried", "mensa-pasing", "mensa-weihenstephan", "stubistro-arcisstr", "stubistro-goethestr", 
+                 "stubistro-großhadern", "stubistro-grosshadern", "stubistro-rosenheim", "stubistro-schellingstr", 
+                 "stucafe-adalbertstr", "stucafe-akademie-weihenstephan", "stucafe-boltzmannstr", "stucafe-garching", 
+                 "stucafe-karlstr", "stucafe-pasing"];
+
+var root = document.getElementById("app");
+
+var locationsDropdown = m("div", {class: "dropdown"}, [
+                            m("div", {class: "dropdown-trigger"},
+                            m("button", {class: "button"},[
+                                m("span", "Select Location"),
+                                m("span", {class: "icon icon-small"},
+                                    m("i", {class: "fa fa-angle-down"}))
+                            ])),
+                            m("div", {class: "dropdown-menu", role: "menu"},
+                                m("div", {class: "dropdown-content"},
+                                    locations.map(function(loc) {return m("a", {href: "#", class: "dropdown-item"}, loc)})))
+                        ]);
+
+m.render(root, [locationsDropdown]);
+
+var dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
