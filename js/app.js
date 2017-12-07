9cfb2335a51b6b7e734029b1b2c3427954019d0e
Simon Rehwald <simon.rehwald@tum.de>
2017-12-08 00:48:09 +0100

    change location text based on selected location

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var locations = ["fmi-bistro", "ipp-bistro", "mensa-arcisstr", "mensa-garching",
                  "stucafe-karlstr", "stucafe-pasing"];
 
 var root = document.getElementById("app");
+var currentLocation = locations[0];
 
 var locationsDropdown = m("div", {class: "dropdown"}, [
                             m("div", {class: "dropdown-trigger"},
@@ -15,10 +16,21 @@ var locationsDropdown = m("div", {class: "dropdown"}, [
                             ])),
                             m("div", {class: "dropdown-menu", role: "menu"},
                                 m("div", {class: "dropdown-content"},
-                                    locations.map(function(loc) {return m("a", {href: "#", class: "dropdown-item"}, loc)})))
+                                    locations.map(function(loc) {return m("a", {href: "#", class: "dropdown-item", 
+                                                                                onclick: function() {setLocation(loc)}}, loc)})))
                         ]);
 
-m.render(root, [locationsDropdown]);
+m.mount(root, {
+    view: function() {
+        return m("div", [locationsDropdown, 
+                         m("div", {class: "has-text-centered"}, 
+                            m("h1", {class: "title"}, currentLocation))]);
+    }
+});
+
+function setLocation(loc) {
+    currentLocation = loc;
+}
 
 var dropdown = document.querySelector('.dropdown');
 dropdown.addEventListener('click', function(event) {
