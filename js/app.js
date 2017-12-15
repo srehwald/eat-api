c989be24d5a3289256bb787e12626a874401a37f
Simon Rehwald <simon.rehwald@tum.de>
2017-12-15 23:06:39 +0100

    prototype for performing menu GET request

--- js/app.js
+++ js/app.js
@@ -7,7 +7,23 @@ var locations = ["fmi-bistro", "ipp-bistro", "mensa-arcisstr", "mensa-garching",
 var root = document.getElementById("app");
 var currentLocation = locations[0];
 
-var locationsDropdown = m("div", {class: "dropdown"}, [
+var MenuData = {
+    menu: null,
+    fetch: function() {
+            m.request({
+                method: "GET",
+                // TODO
+                url: currentLocation + "/2017/49.json"
+            })
+            .then(function(menu) {
+                MenuData.menu = menu;
+            })
+        }
+}
+
+var LocationsDropdown = {
+    view: function() {
+        return m("div", {class: "dropdown"}, [
                             m("div", {class: "dropdown-trigger"},
                             m("button", {class: "button"},[
                                 m("span", "Select Location"),
@@ -18,18 +34,33 @@ var locationsDropdown = m("div", {class: "dropdown"}, [
                                 m("div", {class: "dropdown-content"},
                                     locations.map(function(loc) {return m("a", {href: "#", class: "dropdown-item", 
                                                                                 onclick: function() {setLocation(loc)}}, loc)})))
-                        ]);
-
-m.mount(root, {
-    view: function() {
-        return m("div", [locationsDropdown, 
-                         m("div", {class: "has-text-centered"}, 
-                            m("h1", {class: "title"}, currentLocation))]);
+                        ])
     }
-});
+}
+
+var Menu = {
+    oninit: MenuData.fetch,
+    view: function() {
+        // TODO display all dishes
+        return MenuData.menu ? m("div", MenuData.menu.days[0].dishes[0].name) : m("div", "Loading...")
+    }
+}
+
+var App = {
+    view: function() {
+        // TODO
+        return m("div", [m(LocationsDropdown), 
+                         m("div", {class: "has-text-centered"}, 
+                            m("h1", {class: "title"}, currentLocation)), m(Menu)]);
+    }
+}
+
+
+m.mount(root, App);
 
 function setLocation(loc) {
     currentLocation = loc;
+    MenuData.fetch();
 }
 
 var dropdown = document.querySelector('.dropdown');
