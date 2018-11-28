588c65bb42f992ded0b7b2bf03702e1729697531
Simon Rehwald <simon.rehwald@tum.de>
2018-11-28 19:15:31 +0100

    add mediziner-mensa to parse script

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -7,7 +7,7 @@ loc_list=( "mensa-arcisstr" "mensa-arcisstrasse" "mensa-garching" "mensa-leopold
 "mensa-martinsried" "mensa-pasing" "mensa-weihenstephan" "stubistro-arcisstr" "stubistro-goethestr" \
 "stubistro-großhadern" "stubistro-grosshadern" "stubistro-rosenheim" "stubistro-schellingstr" "stucafe-adalbertstr" \
 "stucafe-akademie-weihenstephan" "stucafe-boltzmannstr" "stucafe-garching" "stucafe-karlstr" "stucafe-pasing" \
-"ipp-bistro" "fmi-bistro" )
+"ipp-bistro" "fmi-bistro" "mediziner-mensa" )
 
 for loc in "${loc_list[@]}"; do
     python src/main.py "$loc" --jsonify "./dist/$loc"
